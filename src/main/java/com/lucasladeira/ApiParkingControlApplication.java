3aa66f77e7914c728a402612fad808142ae0be02
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-02-07 12:47:17 -0300

    :tada: first commit

---
+++ src/main/java/com/lucasladeira/ApiParkingControlApplication.java
@@ -0,0 +1,13 @@
+package com.lucasladeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParkingControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasladeira/ApiParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasladeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
